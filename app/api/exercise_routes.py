77449a1ab1ab5fb1160102d880729c5c353834a2
Vivian Thach <vivianlthach@gmail.com>
2022-02-15 14:12:30 -0800

    link exercises blueprint to flask app and implement READ all exercises

--- app/api/exercise_routes.py
+++ app/api/exercise_routes.py
@@ -3,15 +3,15 @@ from flask_login import current_user
 from app.models import Exercise
 
 
-user_routes = Blueprint('exercises', __name__)
+exercise_routes = Blueprint('exercises', __name__)
 
 
-@user_routes.route('/')
+@exercise_routes.route('/')
 def get_all_exercises():
     exercises = Exercise.query.all()
     return {'exercises': [exercise.to_dict() for exercise in exercises]}
 
 
-@user_routes.route('/', methods=["POST"])
+@exercise_routes.route('/', methods=["POST"])
 def add_exercise():
     pass
