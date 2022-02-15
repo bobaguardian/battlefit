f69167fbef8deecffaf91b2a7530bfd095990908
Vivian Thach <vivianlthach@gmail.com>
2022-02-15 09:26:50 -0800

    Edit user seeds

---
+++ app/models/exercise.py
@@ -0,0 +1,4 @@
+from .db import db
+
+class Exercise(db.Model):
+    __tablename__ = 'exercise'
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,15 +4,9 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='deMOUSEr', email='demouser@aa.io', password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
 
     db.session.commit()
 
