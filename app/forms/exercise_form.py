50a3630ec694088ee31b02c036f87e2b3889a878
Vivian Thach <vivianlthach@gmail.com>
2022-02-16 17:05:05 -0800

    Add error handling for adding/editing with an exercise name that already exists

--- app/forms/exercise_form.py
+++ app/forms/exercise_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, TextAreaField, FileField
+from wtforms import StringField, TextAreaField, FileField
 from wtforms.validators import DataRequired, ValidationError
-from app.models import MuscleGroup
+from app.models import MuscleGroup, Exercise
 
 def muscle_group_exists(form, field):
     muscle_name = field.data
@@ -14,8 +14,14 @@ def name_within_length(form, field):
     if len(name) > 100:
         raise ValidationError("Exercise name must be less than 100 characters.")
 
+def name_is_unique(form, field):
+    name = field.data
+    exercise = Exercise.query.filter(Exercise.name == name).first()
+    if exercise:
+        raise ValidationError("Exercise name already exists.")
+
 class ExerciseForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired(), name_within_length])
+    name = StringField('name', validators=[DataRequired(), name_within_length, name_is_unique])
     # muscle_group = SelectField("muscle-group", choices=[muscle.name for muscle in MuscleGroup.query.all()])
     muscle_group = StringField('muscle-group', validators=[DataRequired(), muscle_group_exists])
     description = TextAreaField('description')
