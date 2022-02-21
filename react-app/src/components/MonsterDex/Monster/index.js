db3744137f1fbd9f1089f93c349d3fc982aa043b
Vivian Thach <vivianlthach@gmail.com>
2022-02-21 08:24:30 -0800

    Restructure monster dex to 3 column grid

--- react-app/src/components/MonsterDex/Monster/index.js
+++ react-app/src/components/MonsterDex/Monster/index.js
@@ -1,10 +1,10 @@
 const Monster = ({id, name, image, description, level}) => {
     return (
         <div className="monster-box">
-            <img src={image} alt={`${name}-monster`}></img>
+            <img className="monster-img" src={image} alt={`${name}-monster`}></img>
             <div className="monster-details">
-                <h3>{name}  no. {id}   level {level}</h3>
-                <p>{description}</p>
+                <h3>No. {id} {name}</h3>
+                {/* <p>{description}</p> */}
             </div>
         </div>
     )
