523de269ebc9a4afe2e2d092e9cddda57a51815c
Vivian Thach <vivianlthach@gmail.com>
2022-02-21 10:33:11 -0800

    Add dates to some battle seeders

--- app/seeds/battles.py
+++ app/seeds/battles.py
@@ -1,13 +1,15 @@
+from datetime import datetime
 from app.models import db, Battle
 
+
 def seed_battles():
-    battle1 = Battle(user_id=2, monster_id=1, defeated=False)
+    battle1 = Battle(user_id=2, monster_id=1, defeated=False, date=datetime(2022, 1, 1))
     battle2 = Battle(user_id=2, monster_id=41, defeated=False)
-    battle3 = Battle(user_id=2, monster_id=38, defeated=False)
+    battle3 = Battle(user_id=2, monster_id=38, defeated=False, date=datetime(2022, 2, 14))
     battle4 = Battle(user_id=2, monster_id=10, defeated=True)
-    battle5 = Battle(user_id=2, monster_id=18, defeated=True)
+    battle5 = Battle(user_id=2, monster_id=18, defeated=True, date=datetime(2022, 1, 19))
     battle6 = Battle(user_id=2, monster_id=53, defeated=False)
-    battle7 = Battle(user_id=2, monster_id=1, defeated=True)
+    battle7 = Battle(user_id=2, monster_id=1, defeated=True, date=datetime(2022, 2, 2))
     battle8 = Battle(user_id=2, monster_id=1, defeated=True)
 
 
