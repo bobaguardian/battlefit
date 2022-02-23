c3982c023971636d6f19614586293f92486f42a3
Vivian Thach <vivianlthach@gmail.com>
2022-02-23 11:26:23 -0800

    Add monster carousel to splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,7 +1,37 @@
+import ImageGallery from 'react-image-gallery';
 import LoginFormModal from "../auth/LoginFormModal";
 import SignUpFormModal from "../auth/SignupFormModal";
 import Footer from "../Footer";
 import "./SplashPage.css";
+
+
+const images = [
+    {
+        original: "https://battle-fit.s3.amazonaws.com/c9944a276ef5454293dc3a0999f61a3f.jpg",
+        thumbnail: "https://battle-fit.s3.amazonaws.com/c9944a276ef5454293dc3a0999f61a3f.jpg",
+        originalClass:"carousel-image"
+    },
+    {
+        original: "https://battle-fit.s3.amazonaws.com/52dca2a1dd3a4654a235bcef951f94d6.jpg",
+        thumbnail: "https://battle-fit.s3.amazonaws.com/52dca2a1dd3a4654a235bcef951f94d6.jpg",
+        originalClass:"carousel-image"
+    },
+    {
+        original: "https://battle-fit.s3.amazonaws.com/47a293bc14684361819f08c675fe02bb.jpg",
+        thumbnail: "https://battle-fit.s3.amazonaws.com/47a293bc14684361819f08c675fe02bb.jpg",
+        originalClass:"carousel-image"
+    },
+    {
+        original: "https://battle-fit.s3.amazonaws.com/c4bc270236e341128996e5715a99e837.jpg",
+        thumbnail: "https://battle-fit.s3.amazonaws.com/c4bc270236e341128996e5715a99e837.jpg",
+        originalClass:"carousel-image"
+    },
+    {
+        original: "https://battle-fit.s3.amazonaws.com/cc9bdfeda5bd436a99845d5d066d760b.png",
+        thumbnail: "https://battle-fit.s3.amazonaws.com/cc9bdfeda5bd436a99845d5d066d760b.png",
+        originalClass:"carousel-image"
+    },
+]
 
 const SplashPage = () => {
 
@@ -32,6 +62,18 @@ const SplashPage = () => {
                 <div id="splash-text">
                     <h2>Welcome to BattleFit!</h2>
                     <p>Keep track of your exercises and logs while defeating and collecting mythical creatures!</p>
+                    <div className="carousel">
+                        <ImageGallery
+                            items={images}
+                            showThumbnails={false}
+                            showFullscreenButton={false}
+                            showPlayButton={false}
+                            autoPlay={true}
+                            slideInterval={5000}
+                            showNav={false}
+                        />
+
+                    </div>
                 </div>
                 <div id="splash-ready" onClick={openSignupFormModal}>Ready to Battle?</div>
             </main>
