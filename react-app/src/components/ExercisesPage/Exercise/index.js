a91985a27ae84999436449ba6397f3d5b184ff97
Vivian Thach <vivianlthach@gmail.com>
2022-02-23 08:20:34 -0800

    Show muscle name on user exercises page but not the others

--- react-app/src/components/ExercisesPage/Exercise/index.js
+++ react-app/src/components/ExercisesPage/Exercise/index.js
@@ -1,12 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 import { removeExercise } from "../../../store/exercises";
 import EditExerciseFormModal from "../EditExerciseFormModal";
 import AddLogFormModal from "../../LogsPage/AddLogFormModal";
 import AddBattleLogFormModal from "../../BattlePage/AddBattleLogFormModal";
 
-const Exercise = ({ id, user_id, name, muscle_group, description, image, monsterName, battleId }) => {
+const Exercise = ({ id, user_id, name, muscle_group, description, image, monsterName, battleId, showMuscle }) => {
 	const dispatch = useDispatch();
+    const location = useLocation();
 	const sessionUser = useSelector((state) => state.session.user);
     const exercise = {id, user_id, name, muscle_group, description, image};
 
@@ -22,7 +24,7 @@ const Exercise = ({ id, user_id, name, muscle_group, description, image, monster
                 {image ? <img className="exercise-image" src={image} alt="exercise-pic"></img> : null}
                 <div className="exercise-detail-container">
                     <h3>{name}</h3>
-                    {muscle_group ? <p>{muscle_group.name}</p> : null}
+                    {showMuscle && muscle_group ? <p>{muscle_group.name}</p> : null}
                     {description ? <p>{description}</p> : null}
                 </div>
 
