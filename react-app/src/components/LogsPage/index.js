4d87e4ae0b6c8d5e85423b91401d73bd9855d615
Vivian Thach <vivianlthach@gmail.com>
2022-02-24 14:47:11 -0800

    Adjust log page border toggle, add slidein and appear to battle page

--- react-app/src/components/LogsPage/index.js
+++ react-app/src/components/LogsPage/index.js
@@ -32,16 +32,18 @@ const LogsPage = () => {
 
         let dateLogs = document.getElementsByClassName(`log-${e.target.id}`)
         for (let i = 0; i < dateLogs.length; i++) {
-            if (dateLogs[i].style.opacity == 0) {
+            if (dateLogs[i].style.opacity === "0") {
                 dateLogs[i].style.opacity = "1";
                 dateLogs[i].style.height = "150px";
                 dateLogs[i].style.overflow = "visible";
                 dateLogs[i].style.padding = "30px";
+                dateLogs[i].style.borderTop = "1px solid rgba(104, 105, 99, 0.8)";
             } else {
                 dateLogs[i].style.opacity = "0";
                 dateLogs[i].style.height = "0px";
                 dateLogs[i].style.overflow = "hidden";
                 dateLogs[i].style.padding = "0px";
+                dateLogs[i].style.borderTop = "none";
 
             }
         }
