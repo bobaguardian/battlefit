d85fb32334ee7da05d4aaab43fd861fc40aee8de
Vivian Thach <vivianlthach@gmail.com>
2022-02-24 09:44:09 -0800

    Add technologies to footer, add github and linkedin icons

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,10 +6,13 @@ const Footer = () => {
             <div className="copyright">
                 <p><i className="fa-solid fa-copyright"></i> BattleFit</p>
             </div>
+            <div className="technologies">
+                <p>Python / Flask / PostgreSQL / SQLAlchemy / JavaScript / React / Redux / AWS S3</p>
+            </div>
             <div className="about-links">
-                <a href='https://github.com/bobaguardian/'>Github</a>
+                <a href='https://github.com/bobaguardian/'><i className="fa-brands fa-github"></i></a>
                 <p>Vivian Thach</p>
-                <a href='https://www.linkedin.com/in/vivianthach1023/'>LinkedIn</a>
+                <a href='https://www.linkedin.com/in/vivianthach1023/'><i className="fa-brands fa-linkedin"></i></a>
             </div>
         </footer>
     );
