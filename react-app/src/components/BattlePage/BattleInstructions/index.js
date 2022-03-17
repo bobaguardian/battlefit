c7f9f6fe1f3307d2fc1e22d6091e14712c9b6bfa
Vivian Thach <vivianlthach@gmail.com>
2022-03-17 12:54:45 -0700

    Change position and font size of battle instructions

--- react-app/src/components/BattlePage/BattleInstructions/index.js
+++ react-app/src/components/BattlePage/BattleInstructions/index.js
@@ -3,7 +3,7 @@ import './BattleInstructions.css';
 const BattleInstructions = () => {
   return (
     <div className="instructions-box">
-      <h3>Battle Instructions</h3>
+      <h4>Battle Instructions</h4>
       <p>Attack the monster by logging the exercises below them.</p>
       <p>There isn't a required unit count, so challenge yourself!</p>
     </div>
